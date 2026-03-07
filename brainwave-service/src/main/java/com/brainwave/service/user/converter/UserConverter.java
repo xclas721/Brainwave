a2ad13b849d7ce66fff585d47f6f0d9fdff682cd
xclas721@gmail.com <xclas721@gmail.com>
2026-03-08 01:54:02 +0800

    fix(api): 更新使用者改用 UserUpdateRequest，後端不接、不處理密碼

--- brainwave-service/src/main/java/com/brainwave/service/user/converter/UserConverter.java
+++ brainwave-service/src/main/java/com/brainwave/service/user/converter/UserConverter.java
@@ -4,9 +4,11 @@ import com.brainwave.core.converter.BaseConverter;
 import com.brainwave.service.user.dto.UserDto;
 import com.brainwave.service.user.entity.UserEntity;
 import com.brainwave.service.user.request.UserRequest;
+import com.brainwave.service.user.request.UserUpdateRequest;
 import com.brainwave.service.user.vo.UserVo;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
 import org.mapstruct.ReportingPolicy;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
@@ -17,4 +19,11 @@ public interface UserConverter extends BaseConverter<UserEntity, UserDto, UserVo
     @Mapping(target = "createdAt", ignore = true)
     @Mapping(target = "updatedAt", ignore = true)
     UserEntity toEntityFromRequest(UserRequest request);
+
+    /** 更新時只寫入 profile 欄位，不碰密碼 */
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "password", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt", ignore = true)
+    void updateEntityFromUpdateRequest(UserUpdateRequest request, @MappingTarget UserEntity entity);
 }
